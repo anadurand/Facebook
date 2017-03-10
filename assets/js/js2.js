cb72a0d0fdf2ff25edaab7a35060b6d553146c81
Ana Durand <ana.dc1004@gmail.com>
2017-03-10 14:37:26 -0500

    cambios en todo

--- assets/js/js2.js
+++ assets/js/js2.js
@@ -3,47 +3,118 @@ window.addEventListener('load',function() {
 
 var muro = [{user: "ana", publicaciones: [{estado: 1, comentario:"hola"},{estado: 0,comentario:"que"}]}];
 
-//var user = location.search.split('nombre=')[1].toUpperCase();
-//document.title = "Facebook - " + user;
+var user = location.search.split('&')[1];
+
+//var id = location.search.split('=')[2];
+//document.title = "Facebook - " + user.toUpperCase();
+//filtra el muro segun usuario
+var muroUser = muro.filter(function(items,j){
+                    return items.user == "ana";
+
+                });
 var publico = document.getElementById("publico");
 var amigos = document.getElementById("amigos");
 var editar = document.getElementsByName("editar");
 var eliminar = document.getElementsByName("eliminar")
-
 var publicaciones = document.getElementById("publicaciones");
+var post = document.getElementById("post");
+
 var frase = "";
 
 publico.addEventListener("click", function(ev){
   ev.preventDefault();
-  muro.forEach(function(item,i){
-        if(item.publicaciones[i].estado == 1){
+  frase = "";
+  muroUser[0].publicaciones.forEach(function(item,i){
+        if(item.estado == 1){
           frase += "<div class = ''>" +
-                    item.publicaciones[i].comentario +
+                    item.comentario +
                     "<br><br>" +
-                    "<a href = '' name = 'editar'>" + "Editar" + "</a>"+
-                    "<a href = '' name = 'eliminar'>Eliminar</a>"+
                     "</div>";
         }
   });
   publicaciones.innerHTML = frase;
-
 });
 
 amigos.addEventListener("click", function(ev){
   ev.preventDefault();
-  muro.forEach(function(item,i){
-        if(item.publicaciones[i].estado == 0){
+  frase = "";
+  muroUser[0].publicaciones.forEach(function(item,i){
+        if(item.estado == 0){
           frase += "<div class = ''>" +
-                    item.publicaciones[i].comentario +
+                    item.comentario +
                     "<br><br>" +
-                    "<a href = '' id = 'editar'>" + "Editar" + "</a>"+
-                    "<a href = '' id = 'eliminar'>Eliminar</a>"+
                     "</div>";
         }
   });
   publicaciones.innerHTML = frase;
 
 });
+
+
+function createPost(texto) {
+
+
+      var post = document.createElement('div');
+      var texto = document.createElement('p');
+      texto.innerHTML = texto;
+      var editar = document.createElement('a');
+      var eliminar = document.createElement('a');
+      editar.innerHTML = "Editar";
+      editar.href = "";
+      eliminar.innerHTML = "Eliminar";
+      eliminar.href = "";
+      editar.addEventListener("click", function(e){
+        e.preventDefault();
+        var postParent = e.target.parent; // Devuelve el padre
+
+      });
+      eliminar.addEventListener('click',function(e) {
+        e.preventDefault();
+        var postParent = e.target.parent; // Devuelve el padre
+      });
+
+      post.appendChild(texto);
+      post.appendChild(editar);
+      post.appendChild(eliminar);
+      return post;
+
+
+
+  }
+
+  post.addEventListener("click", function(e){
+    e.preventDefault();
+    var texto = document.getElementById("texto").value;
+    if(texto == ""){
+      alert("Falta ingresar texto");
+    }else{
+      nuevoPost(texto);
+      publicaciones.appendChild(createPost(texto));
+      texto.innerHTML = "";
+
+    }
+
+
+  });
+
+  function nuevoPost(texto){
+
+    muroUser[0].publicaciones
+
+  }
+
+  function CrearMuro(id){
+
+    muroUser = muro.filter(function(items,j){
+                       return items.user == "ana";
+                      });
+    if(muroUser.length == 0){
+
+
+    }
+
+
+  }
 
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,24 @@
 window.addEventListener('load',function() {
 
-  var users = [{user: "ana",password: "00"}];  //array de objetos
+  var users = [{id: 1, name: "ana",user: "ana.dc1004@gmail.com",password: "00"}];  //array de objetos
   var loginButton = document.getElementById('login');
 
   loginButton.addEventListener('click',function(event) {
     event.preventDefault();
-    var user = document.getElementById("user").value;
-    var password = document.getElementById("password").value;
+    var user = document.getElementById("user");
+    var password = document.getElementById("password");
     var errorUser = document.getElementById("errorUser");
     var errorPassword = document.getElementById("errorPassword");
     var link = document.getElementById("link");
 
-    if(user == "" || password == ""){
+    if(user.value == "" || password.value == ""){
       if (user == "" && password == "") {
         errorUser.innerHTML = "Falta ingresar usuario";
         errorPassword.innerHTML = "Falta ingresar password";
         user.focus();
 
       }else {
-        if(user == ""){
+        if(user.value == ""){
           errorUser.innerHTML = "Falta ingresar usuario";
           errorPassword.innerHTML = "";
           user.focus();
@@ -29,23 +29,29 @@ window.addEventListener('load',function() {
         }
       }
     }else{
+      if(user.value.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.com$/)){
+        users.forEach(function(item){
+          if(item.user == user.value){
+            if(item.password == password.value ){
+              document.location = "index2.html?nombre= &"+ item.name + "&id=" + item.id;
+            }else{
+              errorUser.innerHTML = "";
+              errorPassword.innerHTML = "Password incorrecto";
+              password.focus();
+            }
 
-      users.forEach(function(item){
-        if(item.user == user){
-          if(item.password == password ){
-              document.location = "index2.html?nombre="+user;
           }else{
-            errorUser.innerHTML = "";
-            errorPassword.innerHTML = "Password incorrecto";
-            password.focus();
+            errorUser.innerHTML = "E-mail no existe";
+            errorPassword.innerHTML = "";
+            user.focus();
           }
+        });
 
-        }else{
-          errorUser.innerHTML = "Usuario no existe";
-          errorPassword.innerHTML = "";
-          user.focus();
-        }
-      })
+      }else{
+        errorUser.innerHTML = "Ingresar e-mail correcto";
+        errorPassword.innerHTML = "";
+        user.focus();
+      }
     }
 
   });
